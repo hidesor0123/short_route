6d8cf97ec7ee6bf3602557793f53f118c692ccbd
BenWu <hidesor@yahoo.com.tw>
2017-09-13 00:13:18 +0800

    test

---
+++ short_route/app/src/main/java/com/best/short_route/activity_point.java
@@ -0,0 +1,15 @@
+package com.best.short_route;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_point extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_point);
+    }
+
+    
+}
